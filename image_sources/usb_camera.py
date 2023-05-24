0c2e41859a1fa29a931a0eedc627f254f61b0dff
Achuthaperumal R <achuthaperumal@gmail.com>
2023-05-24 19:39:35 +0530

    Implemented USBCamera Class & Added Unit test

--- image_sources/usb_camera.py
+++ image_sources/usb_camera.py
@@ -1,23 +1,35 @@
+from cv2 import VideoCapture, CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT
+from image_sources.image_source import ImageSource
+
+
 class USBCamera(ImageSource):
     def __init__(self, camera_sdk):
         self.camera_sdk = camera_sdk
+        self.cam = None
 
-    def open(self):
-        # Open USB camera using the camera SDK
-        pass
+    def open(self,cam_id):
+        # Open USB camera using the camera SDK 
+        self.cam = VideoCapture(cam_id)
+        return self.cam.isOpened()
+        
 
     def close(self):
         # Close the USB camera and release resources
-        pass
+        if self.cam != None:
+            self.cam.release()
 
     def acquire_image(self):
         # Acquire image from the USB camera using the camera SDK
-        pass
+        return self.cam.read()
 
     def release_resources(self):
         # Release any resources associated with the USB camera
-        pass
+        self.close()
 
     def get_resolution(self):
         # Get the resolution of the USB camera
-        pass
+        return self.cam.get(CAP_PROP_FRAME_WIDTH), self.cam.get(CAP_PROP_FRAME_HEIGHT)
+    
+    def is_opened(self):
+        return self.cam.isOpened()
+    
