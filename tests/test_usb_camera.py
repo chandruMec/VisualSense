0d2f0d1d5c465ffa6686d9266e65946806a8dea9
Gokul0305 <109408774+Gokul0305@users.noreply.github.com>
2023-06-04 22:13:50 +0530

    Update test_usb_camera.py

--- tests/test_usb_camera.py
+++ tests/test_usb_camera.py
@@ -43,47 +43,4 @@ class TestUSBCamera(unittest.TestCase):
 
 
 if __name__ == '__main__':
-=======
-from image_sources.usb_camera import USBCamera
-import numpy
-
-
-
-class test_usb(unittest.TestCase):
-
-    usb_cam = USBCamera()
-    
-    def test_open(self):
-       self.assertTrue(self.usb_cam.open(0))
-
-    def test_is_opened(self):
-        self.assertTrue(self.usb_cam.is_opened(0))
-
-    def test_camera(self):
-        self.assertIsInstance(self.usb_cam.cameras(),list)
-        print(self.usb_cam.cameras())
-
-    def test_image(self):
-        self.assertIsInstance(self.usb_cam.image(0),numpy.ndarray)
-
- 
-    def test_get_resolution(self):
-       
-        self.assertIsInstance(self.usb_cam.get_resolution(0),tuple)
-        
-
-    def test_close(self):
-        self.usb_cam.close(0)
-        self.assertFalse(self.usb_cam.is_opened(0))
-
-
-   
-    
-
-
-
-
-
-
-if __name__ == "__main__":
     unittest.main()
